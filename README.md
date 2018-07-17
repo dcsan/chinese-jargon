f40a206efda33694d77c8c8ab1f01310773d1d4a
dcsan <dc@rikai.co>
2018-07-17 22:06:08 +0800

    move files

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ https://dcsan.github.io/chinese-jargon/
 There's a script `bin/build.sh` that will basically just copy the rawdata into `docs/data`
 at a later date we will transform the CSV files into more useful formats like JSON etc. and make them easier to display.
 
+## preview the files and data
+you can see published versions on github pages or look at the files in the repo eg
+
+[markdown gamer talk](./docs/data/games.md)
+[markdown net jargon](./docs/data/net.md)
--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-echo "TODO convert to JSON etc"
+
+
+# TODO - transform to JSON etc
 
 mkdir -p docs/data
-cp -r rawdata/* docs/data
+
+echo "copying /rawdata to /docs/data"
+cp "rawdata/tech jargon - games.csv" docs/data/games.csv
+cp "rawdata/tech jargon - net.csv" docs/data/net.csv
+
+# npm install -g csv2md
+
+echo "convert to markdown files"
+csv2md "rawdata/tech jargon - games.csv" > docs/data/games.md
+csv2md "rawdata/tech jargon - net.csv" > docs/data/net.md
